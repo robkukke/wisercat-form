d1473ad1902fe683e9f6d8343ae942f93129df1d
Robin Kukke <robkukke@users.noreply.github.com>
2024-04-09 20:21:24 +0300

    Minor validation fix

--- src/app/commons/utils/custom-validators/custom-validators.util.ts
+++ src/app/commons/utils/custom-validators/custom-validators.util.ts
@@ -3,6 +3,10 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 export class CustomValidatorsUtil {
   static forbiddenMonthsValidator(decimalLimit: number): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
+      if (!control || control.value === null || control.value === undefined || control.value === '') {
+        return null;
+      }
+
       const value = Number(control.value.replace(',', '.'));
 
       if (isNaN(value)) {
